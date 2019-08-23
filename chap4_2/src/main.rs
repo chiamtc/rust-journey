c4a67322c2baef55094e77e06e295fd8b909b93e
tat cheng chiam <chiamcheng@gmail.com>
2019-08-23 22:18:27 +1000

    slice , string and arr slice

---
+++ chap4_2/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    println!("Hello, world!");
+
+    let s = String::from("hello");
+    let w = first_word(&s[..]);
+
+    let my_string = String::from("hello world");
+
+    // first_word works on slices of `String`s
+    let word = first_word(&my_string[..]);
+
+    let my_string_literal = "hello world";
+
+    // first_word works on slices of string literals
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+
+    let a = [1, 2, 3, 4, 5];;
+
+    let slice = &a[1..3];
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
