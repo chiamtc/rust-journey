d1d4b0c5162f766c44f9f0eea36a463c9b704c19
tat cheng chiam <chiamcheng@gmail.com>
2019-10-02 15:53:43 +1000

    chap5_3. struct and impl

--- chap5_2/src/main.rs
+++ chap5_2/src/main.rs
@@ -1,3 +1,4 @@
+/*
 fn main() {
     let width1 = 30;
     let height1 = 50;
@@ -10,4 +11,16 @@ fn main() {
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}
+*/
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+fn main(){
+    let rect1 = Rectangle { width:30 , height:32};
+    println!("rect1 area is: {:?} ", rect1);
 }
