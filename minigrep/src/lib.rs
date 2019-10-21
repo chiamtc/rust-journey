4c5d0285b594fcb3a671eccf867f77c9d33555eb
tat cheng chiam <chiamcheng@gmail.com>
2019-10-21 14:34:56 +1000

    CLI to search a string in a file. case sensitive and insensitive with env_var

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,26 +1,100 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
-pub struct Config{
-    pub query:String,
-    pub filename:String
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_insensitive: bool,
 }
 
 impl Config {
-    pub fn new(args:&[String]) -> Result<Config, &'static str>{
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             //panic!("not enough parameters");
             return Err("not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+        let case_insensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config { query, filename, case_insensitive })
     }
 }
 
-pub fn run(config:Config) -> Result<(), Box<dyn Error>>{
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?; //? checks chapter 9 panic!
+    let results = if config.case_insensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
 
-    println!("With text:\n{}", contents);
+    for line in results {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+//https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html#lifetime-annotations-in-function-signatures
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+//    vec![]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."],
+                   search(query, contents));
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
