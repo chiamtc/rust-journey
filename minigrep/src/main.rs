1936042e984f44b2fb653a918b9207e850a605ac
tat cheng chiam <chiamcheng@gmail.com>
2019-10-21 14:52:13 +1000

    standard error println

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,12 +5,13 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+//        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-    if let Err(e) = minigrep::run(config){
 
+    if let Err(e) = minigrep::run(config){
+        eprintln!("Application error: {}", e);
+        process::exit(1);
     }
 }
