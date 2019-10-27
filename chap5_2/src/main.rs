c9b3cda77f8ef7b000fdad3059e231e2dce8f005
DESKTOP-D6SFLPS\chiamtc <chiamcheng@gmail.com>
2019-10-27 15:46:00 +1000

    practicing struct and impl

--- chap5_2/src/main.rs
+++ chap5_2/src/main.rs
@@ -17,10 +17,50 @@ fn area(width: u32, height: u32) -> u32 {
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
-    height: u32
+    height: u32,
 }
 
-fn main(){
-    let rect1 = Rectangle { width:30 , height:32};
-    println!("rect1 area is: {:?} ", rect1);
+
+#[derive(Debug)]
+struct User {
+    firstName: String,
+    lastName: String,
+    age: u32,
+}
+
+impl User{
+    fn is_larger<'a>(&'a self, user2:&'a User) -> &'a User{
+        if &self.age > &user2.age {
+//            User { firstName:self.firsName, lastName:self.lastName, age:self.age}
+           self
+        }else {
+            user2
+        }
+    }
+
+    fn editage(&mut self, incrementAge:u32) -> &User{
+        self.age = &self.age + incrementAge;
+        self
+    }
+
+    fn larger(self, user2:User) -> User{
+        if self.age > user2 .age {
+            self
+        }else{
+            user2
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 32 };
+
+    let mut user1 = User { firstName: String::from("Tat"), lastName: String::from("Cheng"), age: 14 };
+    let user2 = User { firstName: String::from("Tat"), lastName: String::from("Cheng"), age: 13 };
+//    println!("{:?}",user1.is_larger(&user2));
+    println!("{:?}", user1.editage(1));
+    println!("{:?}",user1.larger(user2));
+//    println!("user details {:?}", user1);
+
+//    println!("rect1 area is: {:?} ", rect1);
 }
