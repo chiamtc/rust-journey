00d7650a0f0259c75d37560662b587a5611f424d
Chiam Tat Cheng <chiamcheng@gmail.com>
2019-10-29 20:38:45 +1000

    if let to not exhaustively pattern matching like match {}

--- office_playground4/src/main.rs
+++ office_playground4/src/main.rs
@@ -1,27 +1,27 @@
 use std::collections::HashMap;
 
 #[derive(Debug)]
-enum Example{
+enum Example {
     Float(f64),
     Int(i32),
-    Text(String)
+    Text(String),
 }
 
 fn main() {
-    let x = vec![1,2,3,4];
+    let x = vec![1, 2, 3, 4];
 
     let mut v = Vec::new();
     v.push(1);
     v.push(2);
     v.push(3);
     v.push(4);
-    for i in &v{
+    for i in &v {
         println!("{}", i)
     }
 
     println!("{:?} {} {}", &v, v.len(), v.capacity());
 
-    println!("{:?}",v.pop().unwrap());
+    println!("{:?}", v.pop().unwrap());
 
     let r = vec![
         Example::Int(12),
@@ -29,17 +29,17 @@ fn main() {
         Example::Text(String::from("abc"))
     ];
 
-    let mut res= Example::Int(0);
-    for i in &r{
-        res = match i{
+  /*  let mut res = Example::Int(0);
+    for i in &r {
+        res = match i {
             Example::Int(a) => {
                 println!("{} an integer", a);
                 Example::Int(*a)
-            },
+            }
             Example::Float(a) => {
-                println!("{} a float number",a);
+                println!("{} a float number", a);
                 continue;
-            },
+            }
             Example::Text(a) => {
                 println!("{} an actual String", a);
                 continue;
@@ -47,25 +47,42 @@ fn main() {
         };
     }
     println!("{:?}", res);
-
+*/
 
     let mut hm = HashMap::new();
     hm.insert(String::from("random"), 12);
     hm.insert(String::from("strings"), 49);
 
 
-    for (k,v) in &hm{
-        println!("{} , {}", k,v)
+    for (k, v) in &hm {
+        println!("{} , {}", k, v)
     }
 
-    match hm.get(&String::from("random")){
-        Some(&n) => println!("{}",n),
+    match hm.get(&String::from("random")) {
+        Some(&n) => println!("{}", n),
+        _ => println!("no match")
+    }
+
+    match hm.get(&String::from("no keys")) {
+        Some(&n) => println!("{}", n),
         _ => println!("no match")
     }
 
     hm.remove(&String::from("strings"));
 
+    let s = Some("c");
+    //this
+    match s{
+        Some(i) => println!("{}", i),
+        _ => {}
+    }
 
+    //equals to this but without the exhaustive matching like match s{}
+    if let Some(i) = s{ //Some(i) is the pattern
+        println!("{}",i);
+    }else{
+        {}
+    }
 }
 
 
