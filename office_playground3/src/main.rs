5f987c12a3e3f5cd8620abd83d89b1bed288fc55
Chiam Tat Cheng <chiamcheng@gmail.com>
2019-10-29 18:59:02 +1000

    enum, matcher and

--- office_playground3/src/main.rs
+++ office_playground3/src/main.rs
@@ -52,10 +52,46 @@ struct Point {
     y: i32,
 }
 
+enum Shape {
+    Rectangle { width: u32, height: u32 },
+    Circle(f64),
+}
+
+impl Shape {
+    fn area(&self) -> f64 {
+        match *self {
+            Shape::Rectangle { width, height } => (width * height) as f64,
+            Shape::Circle(ref r) => 3.14 * (r * r)
+        }
+    }
+}
+
+
+fn division(x: f64, y: f64) -> Option<f64> {
+    if y == 0.0 {
+        None
+    } else {
+        Some(x / y)
+    }
+}
+
 fn main() {
     let u = Direction::Up(Point { x: 0, y: 1 });
     let k = u.match_direction();
     println!("{:?}", k);
     let x = k.destruct();
-    println!("{}", x)
+    println!("{}", x);
+
+    let rect = Shape::Rectangle { width: 2, height: 3 };
+    println!("{}", rect.area());
+
+    let circle = Shape::Circle(4.5);
+    println!("{}", circle.area());
+
+    let res = division(2.0,0.0);
+    let matcher = match res {
+        Some(x) => x,
+        None => 0.0
+    };
+    println!("{}", matcher)
 }
