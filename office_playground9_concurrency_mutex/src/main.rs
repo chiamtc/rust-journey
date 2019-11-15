cb1c8f719bb90b85417714f567577d7887abaece
tat cheng chiam <chiamcheng@gmail.com>
2019-11-15 11:13:46 +1000

    browser engine - parser part 1

--- office_playground9_concurrency_mutex/src/main.rs
+++ office_playground9_concurrency_mutex/src/main.rs
@@ -1,17 +1,17 @@
 use std::sync::{Mutex, Arc};
 use std::thread;
-//mutex = mutual exclusion
+/*mutex = mutual exclusion
 // one thread to access data at one time.
 // the thread that needs to access has to acquire the lock of the mutex
 //once it's done, you need to unlock it so that other thread can access to it.
-// also act as a smart pointer, return mutex guard which implements deference ()
+ also act as a smart pointer, return mutex guard which implements deference ()*/
 
 
 fn main() {
     //Arc = is an atomically referenced counter type,
     // arc converts  the type int primitive type which safe to share across threads.
     let c = Arc::new(Mutex::new(0));
-    let  mut hs = vec![];
+    let mut hs = vec![];
 
     /*
     Whole sequence of this loop =
@@ -19,13 +19,13 @@ fn main() {
     2. each loop spawns a thread and gain the access of the mutex and increase the value by 1
     3. next thread does the same thing in 2.
     */
-    for _ in 0..10{
+    for _ in 0..10 {
         let c = Arc::clone(&c);
         let h = thread::spawn(move || {
             //locking the access exclusively for the thread at this instance
             let mut num = c.lock().unwrap();
             //smart pointer here, so we need to deference it to access the value
-            *num +=1;
+            *num += 1;
             println!("{}", num);
         });
         hs.push(h);
@@ -34,5 +34,5 @@ fn main() {
     for h in hs {
         h.join().unwrap();
     }
-    println!("Result: {}" , *c.lock().unwrap());
+    println!("Result: {}", *c.lock().unwrap());
 }
