96054decfdeff834ce138dff8f5728463ea2725e
Chiam Tat Cheng <chiamcheng@gmail.com>
2019-11-17 18:35:27 +1000

    xmlhttprequest and resposne is good here. need to figure out how to perform onload.

--- wasm-hello-world/index.js
+++ wasm-hello-world/index.js
@@ -2,5 +2,9 @@ const rust = import ('./pkg/wasm_hello_world');
 
 rust.then((func)=>{
     func.create_stuff();
-    func.run_alert("javascript")
+    // func.run_alert("javascript")
+    const ctx = func.instantiate_context();
+
+    // console.log(new AudioContext())
+    console.log(ctx);
 })
--- wasm-hello-world/src/lib.rs
+++ wasm-hello-world/src/lib.rs
@@ -2,6 +2,8 @@ extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
 
+use web_sys::{AudioContext, XmlHttpRequest, console, EventListener};
+
 #[wasm_bindgen]
 //abiltiy to import JS native API ** has to match with actual JS WEB MDN API
 extern "C" {
@@ -11,6 +13,8 @@ extern "C" {
     type Element;
 
     static document: HTMLDocument;
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 
     #[wasm_bindgen(method)]
     fn createElement(this: &HTMLDocument, tagName: &str) -> Element;
@@ -41,6 +45,32 @@ pub fn create_stuff() {
     p.set_inner("hellow");
     div.append(p);
     document.body().append(div);
+}
 
+//good resource : https://github.com/rustwasm/wasm-bindgen/issues/939
+#[wasm_bindgen]
+pub fn instantiate_context() -> Result<web_sys::AudioContext, wasm_bindgen::JsValue> {
+    let ctx = AudioContext::new();
+    let request = XmlHttpRequest::new().unwrap();
+    request.open("GET", "https://firebasestorage.googleapis.com/v0/b/podstetheedata.appspot.com/o/human_samples%2F-Lsxlh74yy4ASUohCFEA.wav?alt=media&token=6088e994-73b6-47a4-bc0d-a1090cb3b288");
+//    let response_type = from_js_value(ArrayBuffer);
+    request.set_response_type(web_sys::XmlHttpRequestResponseType::Arraybuffer);
+    let results = request.send();
+     match results{
+             Err(e) => println!("nothing {:?}", e),
+             Ok(res) => {
+                 println!("{:?}", res);
+                 console::log_1(&"Hello using web-sys".into());
+             }
+     }
+  /*  match request.onload() {
+        None => println!("nothing..."),
+        Some(f) => {
+            println!("?? {:?}", f);
 
+        }
+    }*/
+//    request.send();
+//    request::onload()
+    ctx
 }
