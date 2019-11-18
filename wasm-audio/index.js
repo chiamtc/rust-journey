3713303c8b4afc51f0b7746ae67df77d9cd89645
tat cheng chiam <chiamcheng@gmail.com>
2019-11-18 16:39:26 +1000

    working codes

--- wasm-audio/index.js
+++ wasm-audio/index.js
@@ -1,11 +1,19 @@
 const rust = import ('./pkg/wasm_audio');
 
+/* OG working decoded audio context
 rust.then(m => {
     m.runner().then((data) => {
-        /*const ctx = new AudioContext().decodeAudioData(data);
-        ctx.then(((res)=>{
-            console.log(res)
-        }))*/
+        const buffer = data;
+        const ctx = new m.M3dAudio();
+        ctx.decode(buffer).then((res)=>{
+            console.log('res',res)
+        })
+    })
+}).catch(console.error);
+*/
+
+rust.then(m => {
+    m.runner().then((data) => {
         const buffer = data;
         const ctx = new m.M3dAudio();
         ctx.decode(buffer).then((res)=>{
