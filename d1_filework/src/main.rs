2128de25ee22d5019dba21a22b9570660c1ca8f7
Chiam Tat Cheng <chiamcheng@gmail.com>
2019-12-11 20:45:15 +1000

    custom error using enum and From trait. ? operator as well

--- d1_filework/src/main.rs
+++ d1_filework/src/main.rs
@@ -1,5 +1,29 @@
 use serde_derive::*;
 
+#[derive(Debug)]
+pub enum TransactionError{
+    LoadError(std::io::Error),
+    ParseError(serde_json::Error)
+}
+
+impl From<std::io::Error> for TransactionError{
+    //from() is a requried function to implement from From trait
+    // signature -> from(T) -> Self
+    // Self is a reserved word, the type we're working with right now aka TransactionError since the impl is "for TransactionError"
+    fn from(e:std::io::Error) -> Self{
+        TransactionError::LoadError(e)
+    }
+}
+
+impl From<serde_json::io::Error> for TransactionError{
+    //from() is a requried function to implement from From trait
+    // signature -> from(T) -> Self
+    // Self is a reserved word, the type we're working with right now aka TransactionError since the impl is "for TransactionError"
+    fn from(e:serde_json::io::Error) -> Self{
+        TransactionError::ParseError(e)
+    }
+}
+
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Transaction{
     from : String,
@@ -17,7 +41,6 @@ fn main() {
 }
 
 fn get_transaction(fname: &str) -> Result<Vec<Transaction>, String>{
-//    Err("No trans".to_string());
     let s = match std::fs::read_to_string(fname){
         Ok(v)=> v,
         Err(e) => return Err(e.to_string())
@@ -27,7 +50,21 @@ fn get_transaction(fname: &str) -> Result<Vec<Transaction>, String>{
         Err(e)=> return Err(e.to_string())
     };
     Ok(t)
-//    Ok(Vec::new())
+}
+
+//this func is using custom build transaction error instead of string
+fn get_transaction_type_error(fname: &str) -> Result<Vec<Transaction>, TransactionError>{
+    let s = match std::fs::read_to_string(fname){
+        Ok(v)=> v,
+        Err(e) => return Err(e.into())
+    };
+    let t:Vec<Transaction> = match serde_json::from_str(&s){
+        Ok(t) => t,
+        Err(e)=> return Err(e.into())
+    };
+    Ok(t)
+    //or a shorter version using ? operator, only applicable to Result
+//    Ok(serde_json::from_str(&std::fs::read_to_string(fname)?)?
 }
 
 fn get_transaction_b(fname: &str) -> Result<Vec<Transaction>, String>{
